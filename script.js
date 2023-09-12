af0224aa79aaa7bebe3316695d5a07c55002bf67
Imam Wahyu Widodo <120608486+hajilok@users.noreply.github.com>
2023-09-12 22:29:24 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const autoPlayLoop = function() {
+	const JUMP_SPEED = 50;
+	const DISTANCE_BEFORE_JUMP = 120;
+
+	const instance = window.Runner.instance_;
+	const tRex = instance.tRex;
+
+	if( tRex.jumping ) {
+		requestAnimationFrame(autoPlayLoop);
+		return;
+	}
+
+	const tRexPos = tRex.xPos;
+	const obstacles = instance.horizon.obstacles;
+
+	const nextObstacle = obstacles.find(o => o.xPos > tRexPos);
+
+	if( nextObstacle && ( nextObstacle.xPos - tRexPos ) <= DISTANCE_BEFORE_JUMP ) {
+		tRex.startJump(JUMP_SPEED)
+	}
+
+	requestAnimationFrame(autoPlayLoop);
+}
+
+requestAnimationFrame(autoPlayLoop);
